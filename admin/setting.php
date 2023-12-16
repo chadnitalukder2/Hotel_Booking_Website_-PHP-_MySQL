704f6d888da2da00936443f0905c516e2b1ff510
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-16 19:45:17 +0600

    setting

--- admin/setting.php
+++ admin/setting.php
@@ -34,33 +34,71 @@
                         </div>
                             <!-- ================= -->
                         <h6 class="card-subtitle mb-1 fw-bold">Site Title</h6>
-                        <p class="card-text">Content</p>
+                        <p class="card-text" id="site_title"></p>
                         <h6 class="card-subtitle mb-1 fw-bold">About Us</h6>
-                        <p class="card-text">Content</p>
+                        <p class="card-text" id="site_about"></p>
                     </div>
                 </div>
-                    <!-- Modal -->
+                    <!-- ========General setting Modal========= -->
                     <div class="modal fade" id="General-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                         <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <form>
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="staticBackdropLabel">General Sattings</h5>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class=" mb-3">
+                                            <label  class="form-label ">Site Title</label>
+                                            <input name="site_title" id="site_title_inp" type="text" class="form-control shadow-none" >
+                                        </div>
+                                        <div class="mb-3">
+                                            <label  class="form-label">About us</label>
+                                            <textarea name="site_about"  id="site_about_inp" class="form-control shadow-none"  rows="5"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" onclick="site_title.value = general_data.site_title,site_about.value = general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                                        <button type="button" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                    </div>
                                 </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Understood</button>
-                            </div>
+                            </form>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     </div>
 
 <?php require("inc/scripts.php") ;?>
+<script>
+    let general_data;
+
+    function get_general(){
+        let site_title = document.getElementById('site_title');
+        let site_about = document.getElementById('site_about');
+
+        let site_title_inp = document.getElementById('site_title_inp');
+        let site_about_inp = document.getElementById('site_about_inp');
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "ajax/settings_crud.php", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload= function(){
+           general_data =JSON.parse( this.responseText);
+           
+           site_title.innerText = general_data.site_title;
+           site_about.innerText = general_data.site_about;
+        
+           site_title_inp.value = general_data.site_title;
+           site_about_inp.value = general_data.site_about;
+        }
+
+        xhr.send('get_general');
+    }
+    window.onload = function(){
+        get_general();
+    }
+</script>
 </body>
 </html>
